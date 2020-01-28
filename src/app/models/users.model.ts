1b26c482fe49b710e8431f6cba764840c00aadd8
Francis Murray <franciswmurray@gmail.com>
2020-01-28 22:15:07 +0100

    User, Evaluator and Candidate Interfaces

--- src/app/models/users.model.ts
+++ src/app/models/users.model.ts
@@ -1,7 +1,31 @@
-export interface Evaluator {
+import { GroupAst } from '@angular/animations/browser/src/dsl/animation_ast';
+
+export interface User {
+  _id: string;
   name: string;
   lastname: string;
-  email: string;
   password: string;
-  organisation: string;
+  email: string;
+  confirmed: boolean;
+  type: UserTypeEnum;
 }
+
+enum UserTypeEnum {
+  evaluator = 'Evaluator',
+  candidate = 'Candidate'
+}
+
+export interface Evaluator extends User {
+  user_id: string;
+  organisation: string;
+  groupsInCharge: Array<string>;
+  assignmentsCreated: Array<string>;
+}
+
+export interface Candidate extends User {
+  user_id: string;
+  organisation: string;
+  groupsInCharge: Array<string>;
+  assignmentsCreated: Array<string>;
+}
+

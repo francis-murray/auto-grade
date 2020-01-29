24af0c1c4e2317cb404f890d568aac3289ccee25
Francis Murray <franciswmurray@gmail.com>
2020-01-29 15:12:24 +0100

    add Ios Interface

--- src/app/models/assignment.model.ts
+++ src/app/models/assignment.model.ts
@@ -2,15 +2,20 @@
 export interface Assignment {
   name: string;
   filename: string;
-  author_id: string;
+  author_id?: string;
   description: string;
   deadline: string;
   is_valid: number;
-  ios: Array<string>;
+  ios: Array<Ios>;
   stats: Array<Stat>;
 }
 
 export interface Stat {
   cpu_time: number;
   file_size: number;
+}
+
+export interface Ios {
+  input: string;
+  output: string;
 }

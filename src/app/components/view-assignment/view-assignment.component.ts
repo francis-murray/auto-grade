0466622c9e31cadb3dc3c4256d59dd94dffedf01
Devmohammed <mohammed.ichou@hotmail.fr>
2020-01-30 11:31:01 +0100

    ajout d'un input field pour le choix du groupe

--- src/app/components/view-assignment/view-assignment.component.ts
+++ src/app/components/view-assignment/view-assignment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Evaluator} from '@angular/compiler-cli/src/metadata/evaluator';
 
 @Component({
   selector: 'app-view-assignment',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAssignmentComponent implements OnInit {
 
-  constructor() { }
+  name : string;
+  Evaluator : string;
+  Deadline : Date;
+  Description : string;
+
+  constructor() {
+    this.name = "Test";
+    this.Evaluator = "Mr Test";
+    this.Deadline = new Date();
+    this.Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat convallis scelerisque. Nulla quis neque ac nulla porta consectetur ut semper diam. In hac habitasse platea dictumst. Sed tempor facilisis aliquet. Aliquam non lorem in diam cursus viverra. Donec fermentum mollis nulla, eget elementum nunc gravida faucibus. Integer sollicitudin sagittis quam, tempus dictum nunc varius ut. Quisque finibus ligula a ultrices dictum."
+  }
 
   ngOnInit() {
   }
+
+
+
 
 }

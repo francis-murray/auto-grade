355d7e76521e7af3706c0fcc670fd441528fa8ff
Devmohammed <mohammed.ichou@hotmail.fr>
2020-02-04 09:20:52 +0100

    connect score view to back and modif page

--- src/app/components/view-score/view-score.component.ts
+++ src/app/components/view-score/view-score.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {GroupsService} from "../../services/groups.service";
+import {AssignementsService} from "../../services/assignements.service";
 
 @Component({
   selector: 'app-view-score',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewScoreComponent implements OnInit {
 
-  constructor() { }
+  groupe = {}
+  assignement = {};
+  Exercice = {};
+  groupeselect : string;
+
+  private isSelected:string;
+
+//in the component define a function
+  setColor(value){
+    this.isSelected=value;
+  }
+
+  constructor(private groupsService: GroupsService, private assignementsService: AssignementsService) { }
 
   ngOnInit() {
+    this.groupsService.getallgroupCandidate().subscribe(
+      response => {
+        console.log("dataFromServer: ", response);
+        if (response.status === 0) {
+          this.groupe = response.groups;
+        }
+      },
+      error => {
+        console.error(error);
+      }
+    );
   }
+
+
+  selectedcard($event){
+    console.log(event.srcElement.id);
+    this.Exercice = {};
+    this.groupeselect = event.srcElement.id;
+    this.groupsService.getassignmentscandidateOne(event.srcElement.id).subscribe(response => {
+      console.log(response.group.assignments);
+      this.Exercice = response.group.assignments;
+    })
+  }
+
+
+
+
 
 }
